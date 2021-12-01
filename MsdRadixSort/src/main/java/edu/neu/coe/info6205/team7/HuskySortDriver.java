f6f2fcf836ca85ee86491beab4d33025fb8d500c
Niro Lin <425060468@qq.com>
2021-12-01 16:57:29 -0500

    feat: implement CharSequence and fit with PureHuskySort

---
+++ MsdRadixSort/src/main/java/edu/neu/coe/info6205/team7/HuskySortDriver.java
@@ -0,0 +1,137 @@
+package edu.neu.coe.info6205.team7;
+import edu.neu.coe.huskySort.sort.huskySort.PureHuskySort;
+import edu.neu.coe.huskySort.sort.huskySortUtils.BaseHuskySequenceCoder;
+import edu.neu.coe.huskySort.sort.huskySortUtils.HuskyCoderFactory;
+import edu.neu.coe.huskySort.sort.huskySortUtils.HuskySequenceCoder;
+import edu.neu.coe.info6205.team7.NameByLetter.NameByLetter;
+import edu.neu.coe.info6205.team7.NameBySyllabification.NameBySyllabification;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class HuskySortDriver {
+
+  private static final HuskySequenceCoder<NameByLetter> nameCoderByLetter =
+      new BaseHuskySequenceCoder<NameByLetter>("Name", 2) {
+        @Override
+        public long huskyEncode(NameByLetter nameByLetter) {
+          return HuskyCoderFactory.utf8Coder.huskyEncode((nameByLetter.getName()));
+        }
+      };
+
+  private static final HuskySequenceCoder<NameBySyllabification> nameCoderBySylla =
+      new BaseHuskySequenceCoder<NameBySyllabification>("Name", 2) {
+        @Override
+        public long huskyEncode(NameBySyllabification nameBySyllabification) {
+          return HuskyCoderFactory.utf8Coder.huskyEncode((nameBySyllabification.getName()));
+        }
+      };
+
+  public static void testSylla(List<String> names_txt){
+    NameBySyllabification[] names = new NameBySyllabification[names_txt.size()];
+    for(int i = 0; i < names_txt.size(); i++){
+      names[i] = new NameBySyllabification(names_txt.get(i));
+    }
+
+    Collections.shuffle(Arrays.asList(names));
+    final int sort_times = 10;
+
+//    long normal_count = 0;
+//    for (int i = 0; i < sort_times; i++) {
+//      NameBySyllabification[] temp = names.clone();
+//      long t1 = System.nanoTime();
+//      Arrays.sort(temp);
+//      long t2 = System.nanoTime();
+//      normal_count += (t2 - t1);
+//    }
+//    System.out.println("System sort: " + normal_count / 1e6 / sort_times + "ms");
+
+    long husky_count = 0;
+    for (int i = 0; i < sort_times; i++) {
+      NameBySyllabification[] temp = names.clone();
+      long t1 = System.nanoTime();
+      PureHuskySort<NameBySyllabification> sorter =
+          new PureHuskySort<>(nameCoderBySylla,
+              false,
+              false);
+      sorter.sort(temp);
+      long t2 = System.nanoTime();
+      husky_count += (t2 - t1);
+    }
+    System.out.println("Husky sort: " + husky_count / 1e6 / sort_times + "ms");
+  }
+
+  public static void testLetter(List<String> names_txt){
+    NameByLetter[] names = new NameByLetter[names_txt.size()];
+    for(int i = 0; i < names_txt.size(); i++){
+      names[i] = new NameByLetter(names_txt.get(i));
+    }
+
+    Collections.shuffle(Arrays.asList(names));
+    final int sort_times = 10;
+
+//    long normal_count = 0;
+//    for (int i = 0; i < sort_times; i++) {
+//      NameByLetter[] temp = names.clone();
+//      long t1 = System.nanoTime();
+//      Arrays.sort(temp);
+//      long t2 = System.nanoTime();
+//      normal_count += (t2 - t1);
+//    }
+//    System.out.println("System sort: " + normal_count / 1e6 / sort_times + "ms");
+
+    long husky_count = 0;
+    for (int i = 0; i < sort_times; i++) {
+      NameByLetter[] temp = names.clone();
+      long t1 = System.nanoTime();
+      PureHuskySort<NameByLetter> sorter =
+          new PureHuskySort<>(nameCoderByLetter,
+              false,
+              false);
+      sorter.sort(temp);
+      long t2 = System.nanoTime();
+      husky_count += (t2 - t1);
+    }
+    System.out.println("Husky sort: " + husky_count / 1e6 / sort_times + "ms");
+  }
+
+  public static void demo() {
+    List<String> names_txt = new ArrayList<>();
+    try{
+      FileReader fr = new FileReader("./sortedChinese.txt");
+      BufferedReader br = new BufferedReader(fr);
+      System.out.println("\n======== Create file reader success! ========");
+
+      while(br.ready()){
+        names_txt.add(br.readLine());
+      }
+
+      System.out.println("======== Data have been read! ========");
+
+      br.close();
+      fr.close();
+    } catch (Exception e){
+      e.printStackTrace();
+    }
+
+    System.out.println("\nTesting name sort by letter:");
+    testLetter(names_txt);
+
+    System.out.println("\nTesting name sort by syllabification:");
+    testSylla(names_txt);
+
+//    try {
+//      FileWriter fr = new FileWriter("./sortedResult.txt");
+//      for (NameBySyllabification name : names) {
+//        fr.write(name.getName() + "\n");
+//      }
+//      fr.close();
+//    }
+//    catch (Exception e){
+//      e.printStackTrace();
+//    }
+  }
+}
--- MsdRadixSort/src/test/java/edu/neu/coe/info6205/team7/NameBySyllabificationTest.java
+++ MsdRadixSort/src/test/java/edu/neu/coe/info6205/team7/NameBySyllabificationTest.java
@@ -10,12 +10,10 @@ public class NameBySyllabificationTest {
   @Test
   public void testTransfer1(){
     NameBySyllabification name1 = new NameBySyllabification("李白");
-    name1.TransferChineseToPinyin();
     assertEquals(name1.getNamePinyin().get(0), "li3");
     assertEquals(name1.getNamePinyin().get(1), "bai2");
 
     NameBySyllabification name2 = new NameBySyllabification("杜甫");
-    name2.TransferChineseToPinyin();
     assertEquals(name2.getNamePinyin().get(0), "du4");
     assertEquals(name2.getNamePinyin().get(1), "fu3");
   }
@@ -23,13 +21,11 @@ public class NameBySyllabificationTest {
   @Test
   public void testTransfer2(){
     NameBySyllabification name1 = new NameBySyllabification("王昌龄");
-    name1.TransferChineseToPinyin();
     assertEquals(name1.getNamePinyin().get(0), "wang2");
     assertEquals(name1.getNamePinyin().get(1), "chang1");
     assertEquals(name1.getNamePinyin().get(2), "ling2");
 
     NameBySyllabification name2 = new NameBySyllabification("欧阳修");
-    name2.TransferChineseToPinyin();
     assertEquals(name2.getNamePinyin().get(0), "ou1");
     assertEquals(name2.getNamePinyin().get(1), "yang2");
     assertEquals(name2.getNamePinyin().get(2), "xiu1");
@@ -37,16 +33,12 @@ public class NameBySyllabificationTest {
 
   @Test
   public void testSpilt1(){
-    NameBySyllabification name1 = new NameBySyllabification("白居易");
-    name1.TransferChineseToPinyin();
-    name1.SpiltBySyllabification();
-    assertEquals(name1.getNameSplitPinyin().get(0), Arrays.asList("b", "ai", "", "2"));
-    assertEquals(name1.getNameSplitPinyin().get(1), Arrays.asList("j", "u", "", "1"));
-    assertEquals(name1.getNameSplitPinyin().get(2), Arrays.asList("y", "i", "", "4"));
+    NameBySyllabification name1 = new NameBySyllabification("窦子昂");
+    assertEquals(name1.getNameSplitPinyin().get(0), Arrays.asList("d", "ou", "", "4"));
+    assertEquals(name1.getNameSplitPinyin().get(1), Arrays.asList("z", "i", "", "5"));
+    assertEquals(name1.getNameSplitPinyin().get(2), Arrays.asList("ang", "", "", "2"));
 
     NameBySyllabification name2 = new NameBySyllabification("苏东坡");
-    name2.TransferChineseToPinyin();
-    name2.SpiltBySyllabification();
     assertEquals(name2.getNameSplitPinyin().get(0), Arrays.asList("s", "u", "", "1"));
     assertEquals(name2.getNameSplitPinyin().get(1), Arrays.asList("d", "ong", "", "1"));
     assertEquals(name2.getNameSplitPinyin().get(2), Arrays.asList("p", "o", "", "1"));
@@ -55,8 +47,6 @@ public class NameBySyllabificationTest {
   @Test
   public void testSpilt2(){
     NameBySyllabification name1 = new NameBySyllabification("祝无双");
-    name1.TransferChineseToPinyin();
-    name1.SpiltBySyllabification();
     assertEquals(name1.getNameSplitPinyin().get(0), Arrays.asList("zh", "u", "", "4"));
     assertEquals(name1.getNameSplitPinyin().get(1), Arrays.asList("w", "u", "", "2"));
     assertEquals(name1.getNameSplitPinyin().get(2), Arrays.asList("sh", "u", "ang", "1"));
@@ -71,16 +61,8 @@ public class NameBySyllabificationTest {
   @Test
   public void testCompare1(){
     NameBySyllabification name1 = new NameBySyllabification("李白");
-    name1.TransferChineseToPinyin();
-    name1.SpiltBySyllabification();
-
     NameBySyllabification name2 = new NameBySyllabification("李太白");
-    name2.TransferChineseToPinyin();
-    name2.SpiltBySyllabification();
-
     NameBySyllabification name3 = new NameBySyllabification("杜甫");
-    name3.TransferChineseToPinyin();
-    name3.SpiltBySyllabification();
 
     assertEquals(name1.compareTo(name2), -1);
     assertEquals(name1.compareTo(name3), 1);
@@ -89,19 +71,11 @@ public class NameBySyllabificationTest {
 
   @Test
   public void testCompare2(){
-    NameBySyllabification name1 = new NameBySyllabification("祝无双");
-    name1.TransferChineseToPinyin();
-    name1.SpiltBySyllabification();
+    NameBySyllabification name1 = new NameBySyllabification("白虎兵");
+    NameBySyllabification name2 = new NameBySyllabification("白华林");
+    NameBySyllabification name3 = new NameBySyllabification("窦子昂");
 
-    NameBySyllabification name2 = new NameBySyllabification("白展堂");
-    name2.TransferChineseToPinyin();
-    name2.SpiltBySyllabification();
-
-    NameBySyllabification name3 = new NameBySyllabification("祝无双双");
-    name3.TransferChineseToPinyin();
-    name3.SpiltBySyllabification();
-
-    assertEquals(name1.compareTo(name2), 1);
+    assertEquals(name1.compareTo(name2), -1);
     assertEquals(name1.compareTo(name3), -1);
     assertEquals(name2.compareTo(name3), -1);
   }
