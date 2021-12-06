530a4a99abf4ed11e99ee2808e1f2266028fac19
Niro Lin <425060468@qq.com>
2021-12-05 23:28:21 -0500

    fix: benchmark run times

--- MsdRadixSort/src/main/java/edu/neu/coe/info6205/team7/Benchmark/BenchmarkChineseSort.java
+++ MsdRadixSort/src/main/java/edu/neu/coe/info6205/team7/Benchmark/BenchmarkChineseSort.java
@@ -25,7 +25,7 @@ import java.util.function.UnaryOperator;
 
 public class BenchmarkChineseSort {
 
-  int nRuns = 1;
+  int nRuns = 500;
   int[] datasetSize = {250000, 500000, 1000000, 2000000, 4000000};
   public final static TimeLogger[] timeLoggersLinearithmic = {
       new TimeLogger("Raw time per run (mSec): ", (time, n) -> time),
